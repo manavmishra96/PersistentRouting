c6b5bb768c8dbc85b6d1db53c7ffc50d0bd59b3f
Manav Mishra <manavmishra96@gmail.com>
2023-06-26 17:43:29 +0530

    modifications

--- mdp/value_iteration.py
+++ mdp/value_iteration.py
@@ -21,7 +21,7 @@ def transitions(state, action, next_state):
 
 
 def rewards(state, action, next_state):
-    global states, loc_space, clock_space, k_space
+    global states, loc_space, clock_space, k_space, poses
     indexer = np.arange(len(states)).reshape(len(loc_space), clock_space.shape[0], clock_space.shape[1], len(k_space))
     
     curr_loc, curr_clock_i, curr_clock_j, curr_k = np.transpose(np.where(indexer == state))[0]
@@ -59,33 +59,74 @@ def value_iteration(states, actions, discount_factor = 0.95, epsilon=1e-6, max_i
 
     print("Policy Rollout...")
     policy = np.zeros(num_states, dtype=int)
-    for state in states:
-        q_values = np.zeros(num_actions)
+    # for state in states:
+    #     q_values = np.zeros(num_actions)
 
-        # Calculate Q-value for each action
-        for action in actions:
-            for next_state in states:
-                transition_prob = transitions[state, action, next_state]
-                immediate_reward = rewards[state, action, next_state]
-                q_values[action] += transition_prob * (immediate_reward + discount_factor * V[next_state])
+    #     # Calculate Q-value for each action
+    #     for action in actions:
+    #         for next_state in states:
+    #             transition_prob = transitions[state, action, next_state]
+    #             immediate_reward = rewards[state, action, next_state]
+    #             q_values[action] += transition_prob * (immediate_reward + discount_factor * V[next_state])
 
-        policy[state] = np.argmax(q_values)  # Select the action with the maximum Q-value
+    #     policy[state] = np.argmax(q_values)  # Select the action with the maximum Q-value
 
     return V, policy
 
 
+def walk(loc, clk, k, opt_policy):
+    counter = 0
+    while counter < 15:
+        counter += 1
+        print(f"Current location: {loc}, Current clock readings: {clk}, no. of targets visited: {k}")
+        action = int(opt_policy[loc, np.where(np.all(clk == clock_space, axis = 1))[0][0], k])
+        print("Optimal action: ", action)
+        loc, clk, k = transition_fxn(loc, clk, k, action)
+        print(f"New location: {loc}, Updated clock readings: {clk}, no. of targets visited: {k}")
+        print('---------------------')
+    return
+
+
+def transition_fxn(location, clocks, k_visited, action, K=4):
+    # increment each clock by 1
+    clocks += 1
+
+    # if any clock reading exceeds the upper limit, clip it to the upper limit
+    clocks = np.clip(clocks, a_min = 0, a_max = K)
+
+    # when the number of visits has become equal to the maximum number of visits, the agent can only go to the depot
+    if k_visited == K:
+        k_visited = 0
+        location = 0
+    
+    else:
+        k_visited += 1
+        location = action
+        
+    clocks[location] = 0 # reset the clock reading of the new location to 0
+    
+    return location, clocks, k_visited
+
+
 if __name__ == "__main__":
-    n = 3
-    K = 7
+    n = 2
+    K = 4
     T = 7
     
     loc_space = np.arange(n + 1)
     k_space = np.arange(K + 1)
     clock_space = np.array(list(itertools.product(np.arange(T+1), repeat=n)))
     
+    poses = np.array([[0,0], [1,0], [2,0]])
+    
     states = np.arange((len(loc_space) * clock_space.shape[0] * clock_space.shape[1] * len(k_space)))
     actions = np.arange(n + 1)
     
     value, policy = value_iteration(states, actions)
-    print(policy)
+    
+    # simulate the agent's walk for 20 steps
+    loc0 = 0
+    clk0 = np.zeros(n + 1)
+    k0 = 0
+    walk(loc0, clk0, k0, policy)
      
